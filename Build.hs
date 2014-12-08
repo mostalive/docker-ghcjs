929ecbe1fbffa63c1fca7274af8eaea298fd2fd0
Willem van den Ende <willem@qwan.eu>
2014-12-08 14:16:16 +0000

    [Refactoring] making string duplication more obvious
    Split the path strings, replacing all "/" separators with the </>
    operator.

--- Build.hs
+++ Build.hs
@@ -1,12 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 import Development.Shake
-import Development.Shake.Command
 import Development.Shake.FilePath
-import Development.Shake.Util
-import Data.Set
-import Data.List as L
-import Data.Typeable
-import GHC.IO.Exception
 
 -- |Track the uuid of an image, so we can rebuild our part of the chain if a FROM container outside of our project changes
 trackImage :: FilePath -> String -> Action ()  
@@ -23,7 +17,7 @@ buildContainer uuidFile imageName dockerFilePath = do
 main :: IO ()
 main =
   shakeArgs shakeOptions{shakeFiles="_build/"} $ do
-    want ["images/ghcjs-cabal.uuid","images/ghcjs-boot.uuid"]
+    want ["images" </> "ghcjs-cabal.uuid", "images" </> "ghcjs-boot.uuid"]
 
     phony "clean" $ do
         putNormal "Cleaning files in _build"
@@ -32,11 +26,11 @@ main =
     "images/haskell-7.8.uuid" %> \out -> do
         alwaysRerun
         trackImage out "haskell:7.8"
-    
+
     "images/ghcjs-cabal.uuid" *> \uuidFile -> do
-        need ["images/haskell-7.8.uuid","ghcjs-cabal/Dockerfile"]
-        buildContainer uuidFile "atddio/ghcjs-cabal" "ghcjs-cabal" 
- 
+        need ["images" </> "haskell-7.8.uuid","ghcjs-cabal" </> "Dockerfile"]
+        buildContainer uuidFile ("atddio" </> "ghcjs-cabal") "ghcjs-cabal" 
+
     "images/ghcjs-boot.uuid" *> \uuidFile -> do
-        need ["images/ghcjs-cabal.uuid", "ghcjs-boot/Dockerfile"]
-        buildContainer uuidFile "atddio/ghcjs-boot" "ghcjs-boot"
+        need ["images" </> "ghcjs-cabal.uuid", "ghcjs-boot" </> "Dockerfile"]
+        buildContainer uuidFile ("atddio" </> "ghcjs-boot") "ghcjs-boot"
